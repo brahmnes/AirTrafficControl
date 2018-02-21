89560fba78077fcaec0d2c73781ef5171a2f0f68
Karol Zadora-Przylecki <karolz@microsoft.com>
2018-02-21 10:47:39 -0800

    Add time passage

--- README.md
+++ README.md
@@ -2,7 +2,10 @@ Setup
 ----
 
 1. Create Azure storage account (to store world state, i.e. information about all flying airplanes)
-1. blah
+1. Ensure that storage connection string in atcsvc/appsettings.json points to the correct storage account
+1. Set AZURE_STORAGE_ACCOUNT_KEY environment variable with the storage account key in the application properties in VS 
+    * It goes into .user file, which is excluded from Git, but not encrypted
+    * You have to "refresh" the project in Visual Studio for Mac for the changes to take effect
 
 
 Setup for Kubernetes (AKS) deployment
@@ -10,5 +13,5 @@ Setup for Kubernetes (AKS) deployment
 1. Create AKS cluster
 1. If you have not created it yet--create an Azure container registry (ACR)
 1. Ensure that AKS service principal has access to the ACR registry 
-  * (it is easy to set it through the Azure portal)
-  * Reader permission should suffice, but it might be necessary to grant AKS a Contributor role
+    a. (it is easy to set it through the Azure portal)
+    b. Reader permission should suffice, but it might be necessary to grant AKS a Contributor role
--- atcsvc/Startup.cs
+++ atcsvc/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,11 +9,23 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Azure.Storage;
 
 namespace atcsvc
 {
     public class Startup
     {
+        private enum TimePassageHandling: int
+        {
+            Completed = 0,
+            InProgress = 1
+        }
+        private const int InvalidTime = -1;
+        private readonly TimeSpan WorldTimerPeriod = TimeSpan.FromSeconds(5);
+
+        private Timer worldTimer_;
+        private int timePassageHandling_ = (int) TimePassageHandling.Completed;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,7 +40,7 @@ namespace atcsvc
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +48,55 @@ namespace atcsvc
             }
 
             app.UseMvc();
+
+            appLifetime.ApplicationStarted.Register(OnApplicationStarted);
+            appLifetime.ApplicationStopping.Register(OnApplicationStopping);
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                appLifetime.StopApplication();
+                // Don't terminate the process immediately, wait for the Main thread to exit gracefully.
+                args.Cancel = true;
+            };
+        }
+
+        private void OnApplicationStarted()
+        {
+            worldTimer_?.Dispose();
+            worldTimer_ = new Timer(OnTimePassed, null, TimeSpan.FromSeconds(1), WorldTimerPeriod);
+        }
+
+        private void OnApplicationStopping()
+        {
+            worldTimer_.Dispose();
+            worldTimer_ = null;
+        }
+
+        private void OnTimePassed(object state)
+        {
+            Task.Run(async () => 
+            {
+                // int currentTime = InvalidTime;
+
+                if (Interlocked.CompareExchange(ref timePassageHandling_, (int) TimePassageHandling.InProgress, (int) TimePassageHandling.Completed) == (int) TimePassageHandling.InProgress)
+                {
+                    // Time passage handling took longer than expected, let bail out and wait for next timer tick.
+                    return;
+                }
+
+                try
+                {
+                    string storageAccountConnectionStringPrefix = Configuration["AzureStorageConnectionString"];
+                    string storageAccountKey = Configuration["AZURE_STORAGE_ACCOUNT_KEY"];
+                    string storageAccountConnectionString = $"{storageAccountConnectionStringPrefix};AccountKey={storageAccountKey}";
+                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+
+                    await Task.CompletedTask;
+                }
+                finally 
+                {
+                    timePassageHandling_ = (int) TimePassageHandling.Completed;
+                }
+            });
         }
     }
 }
