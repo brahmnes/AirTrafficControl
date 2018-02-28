2fb4add305fb6960726976a1f7657cbcf1bbfb92
Karol Zadora-Przylecki <karolz@microsoft.com>
2018-02-28 10:52:01 -0800

    Fix crash in atcsvc

--- atcsvc/Program.cs
+++ atcsvc/Program.cs
@@ -13,6 +13,7 @@ namespace atcsvc
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
     }
 }
