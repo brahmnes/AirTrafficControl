e6dac08be7d3e107ef22ff945bb4e07b96c4cd55
Karol Zadora-Przylecki <karolz@microsoft.com>
2018-03-08 15:32:15 -0800

    Add a private copy of AppMetrics for debugging

---
+++ App.Metrics.AspNetCore.Abstractions/IRouteNameResolver.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IRouteNameResolver.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing;
+
+namespace App.Metrics.AspNetCore
+{
+    public interface IRouteNameResolver
+    {
+        Task<string> ResolveMatchingTemplateRouteAsync(RouteData routeData);
+    }
+}
---
+++ atc.utilities/AppMetrics/MetricsWebHostOptions.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="MetricsWebHostOptions.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System;
+using System.Threading.Tasks;
+using App.Metrics.AspNetCore.Endpoints;
+using App.Metrics.AspNetCore.Tracking;
+
+namespace App.Metrics.AspNetCore {
+    /// <summary>
+    ///     Provides programmatic configuration for metrics, metrics endpoints and tracking middleware in the App Metrics
+    ///     framework.
+    /// </summary>
+    public class MetricsWebHostOptions {
+        public MetricsWebHostOptions() {
+            EndpointOptions = options => { };
+            TrackingMiddlewareOptions = options => { };
+        }
+
+        /// <summary>
+        ///     Gets or sets <see cref="Action{T}" /> to configure the provided <see cref="MetricEndpointsOptions" />.
+        /// </summary>
+        public Action<MetricEndpointsOptions> EndpointOptions { get; set; }
+
+        /// <summary>
+        ///     Gets or sets <see cref="Action{MetricsWebTrackingOptions}" /> to configure the provided
+        ///     <see cref="MetricsWebTrackingOptions" />.
+        /// </summary>
+        public Action<MetricsWebTrackingOptions> TrackingMiddlewareOptions { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the <see cref="EventHandler" /> registered with an exception is thrown.
+        /// </summary>
+        public EventHandler<UnobservedTaskExceptionEventArgs> UnobservedTaskExceptionHandler { get; set; }
+    }
+}
